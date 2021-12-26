b7ea975e4f43f5a9b0c3d8b2521018b2b1b77c4d
Buck4437 <Buck4437@users.noreply.github.com>
2021-12-26 18:03:23 +0800

    day 02

---
+++ Answers/Day02/inputgen.py
@@ -0,0 +1,13 @@
+import random
+
+random.seed(349734922020)
+
+with open("Day02.txt", "w") as f:
+    prev = set()
+    for i in range(1000):
+        while True:
+            num = random.randrange(10000, 99999)
+            if num not in prev:
+                prev.add(num)
+                break
+        f.write(str(num) + "\n")
---
+++ Answers/Day02/main.py
@@ -0,0 +1,16 @@
+with open("Day02.txt") as f:
+    vals = [int(x) for x in f.readlines()]
+
+s1 = s2 = 0
+for val in vals:
+    # Does not ends with 0
+    if val % 10 != 0:
+        s1 += val
+    tmp = val
+    while tmp != 0:
+        if tmp % 10 == 0:
+            break
+        tmp //= 10
+    else:
+        s2 += val
+print(s1, s2)
