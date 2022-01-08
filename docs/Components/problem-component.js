0f10ab8e82ca0120cd018256be57d449631d2c74
Buck4437 <Buck4437@users.noreply.github.com>
2022-01-08 18:31:43 +0800

    added localStorage support

--- docs/Components/problem-component.js
+++ docs/Components/problem-component.js
@@ -9,18 +9,40 @@ Vue.component("problem-component", {
         return {
             stars: 0,
             input: "",
+            saveName: "Buck4437_AoC_Lite_2021_stars"
         }
     },
     mounted() {
         document.title = `Day ${this.day} - Advent of Code Lite`;
+
+        let data = this.getData();
+
+        if (data[this.day]) {
+            this.stars = data[this.day];
+        }
+
         document.addEventListener("keydown", event => {
             if (event.code === "Enter") {
                 this.submit();
             }
         });
-        this.$refs.input.focus()
+        this.$refs.input.focus();
+    },
+    watch: {
+        stars(star) {
+            let data = this.getData();
+            data[this.day] = Math.max(star, data[this.day]|| 0);
+            localStorage.setItem(this.saveName, JSON.stringify(data))
+        }
     },
     methods: {
+        getData() {
+            try {
+                return JSON.parse(localStorage.getItem(this.saveName)) || {};
+            } catch(e) {
+                return {};
+            }
+        },
         hide(...rest) {
             [...rest].forEach(n => this.$el.querySelector(n).classList.add("hidden"))
         },
@@ -51,7 +73,7 @@ Vue.component("problem-component", {
 
         <div v-if="stars > 0" class="part2">
             <p>Your puzzle answer was <code id="part1-ans-val">{{p1Ans}}</code>.</p>
-            <p v-if="stars === 1"><em style="color:#ffff00">The first part of the puzzle is complete! It provides one gold star: *</em></p>
+            <p v-if="stars === 1"><em class="star">The first part of the puzzle is complete! It provides one gold star: *</em></p>
             <h2>--- Part Two ---</h2>
             <slot name="part2">Part 2 content</slot>
             <p v-if="stars === 1">Although it hasn't changed, you can still <a class="link" href="./input.html" target="_blank">get your puzzle input</a>.</p>
@@ -61,7 +83,7 @@ Vue.component("problem-component", {
 
         <div v-if="stars === 2">
             <p>Your puzzle answer was <code>{{p2Ans}}</code>.</p>
-            <p><em style="color:#ffff00">Both parts of this puzzle are complete! They provide two gold stars: **</em></p>
+            <p><em class="star">Both parts of this puzzle are complete! They provide two gold stars: **</em></p>
             <p>At this point, you should <a class="link" href="../index.html">return to your Advent calendar</a> and try another puzzle.</p>
             <p>If you still want to see it, you can <a class="link" href="./input.html" target="_blank">get your puzzle input</a>.</p>
         </div>
